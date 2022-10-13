f2c26383a1ac84fc9b3140469edfbf585aeb60c4
George Kent Allen <programmer@DESKTOP-7SBFESK.localdomain>
2022-10-13 07:54:08 -0700

    Mapper now generalizes to any function.

--- game.py
+++ game.py
@@ -9,14 +9,17 @@ import scipy
 from scipy.integrate import quad
 
 class integralMap:
-    def __init__(self, numOfQuestions):
+    def __init__(self, numOfQuestions, f, start, end):
+        self.__totalArea = quad(f, start, end)[0]
         self.__divisions = [] # list of points on the map
-        self.__integralSliceLength = 1/numOfQuestions #Length of each sub interval
-        print("test")
-        f = lambda x: -numpy.log(x) # Function which detrmines the distrobution of weights
-        for i in range(0, numOfQuestions-1):
+        self.__integralSliceLength = (end-start)/numOfQuestions #Length of each sub interval
+        for i in range(0, numOfQuestions):
+            print(i)
             sliceProgress = self.__integralSliceLength * (i+1) 
-            self.__divisions.append(quad(f, 0, sliceProgress)[0])
+            self.__divisions.append(quad(f, start, sliceProgress)[0]/self.__totalArea)
+        
+        for x in self.__divisions:
+            print(x)
 
     def pickRandomIndex(self):
         index = 0
@@ -27,15 +30,15 @@ class integralMap:
             index = index + 1
         return index
 
-    def __function(x):
-        return numpy.log(x) 
-hype = integralMap(100)
+f = lambda x: 4*x
+
+hype = integralMap(2, f, 0, 1.5)
 ls = []
-for x in range(0, 100):
+for x in range(0, 4):
     ls.append(0)
 for x in range(0, 100000):
     index = hype.pickRandomIndex()
     ls[index] = ls[index] + 1
 
-for x in range(0, 100):
+for x in range(0, 4):
     print(ls[x])
